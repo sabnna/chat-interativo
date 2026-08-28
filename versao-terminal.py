d248706169b23d346423d8a9b3d9eb47ee16ba05
sabnna <pinkprismsa@gmail.com>
2026-08-28 19:59:58 -0300

    add terminal version

---
+++ versao-terminal.py
@@ -0,0 +1,6 @@
+nome=input('Qual o seu nome?')
+print ('Olá', nome, 'é um prazer te conhecer, seja bem vindo (a)')
+origem=input ('De onde você é?')
+local=input ('Que legal que voce é de'+ origem+ ', onde fica essa cidade?') 
+print ('Show! nunca tive a chance de conhecer', local, ', espero ir lá um dia')
+ 
